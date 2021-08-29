0469253bae97d692f33650e011e84a452c8dc176
Shamreen <sham.qureshi37@gmail.com>
2021-08-29 22:04:47 +0530

    modified

--- common.php
+++ common.php
@@ -1,6 +1,6 @@
 <?php
 
-$con= mysqli_connect("remotemysql.com","N765mpbYUe"," Ab3MQoTJuv","N765mpbYUe")
+$con= mysqli_connect("remotemysql.com","N765mpbYUe","Ab3MQoTJuv","N765mpbYUe")
 or die(mysqli_error($con));
 
 ?>

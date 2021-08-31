27ce307b2f1dafb1f244b0aca1b32cba4add3ffd
Shamreen <sham.qureshi37@gmail.com>
2021-08-31 13:08:21 +0530

    modified

--- js/coutndown.js
+++ js/coutndown.js
@@ -3,7 +3,7 @@ $(function() {
 
     // Set the date we're counting down to
 
-    var countDownDate = new Date("aug 31, 2021 0:00:05").getTime();
+    var countDownDate = new Date("sep 1, 2021 0:00:05").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
@@ -37,6 +37,7 @@ $(function() {
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("clock-c").innerHTML = "EXPIRED";
+            document.getElementById("rclock-c").innerHTML = "EXPIRED";
         }
     }, 1000);
 
